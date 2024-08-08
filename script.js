4b853dc41bdd17532bd1e124fa266ccd52460420
MysticalRobot <144855972+DhruvisMystical@users.noreply.github.com>
2024-08-08 16:32:20 -0500

    bruh used wrong account the last time

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+class Wordbruh {
+
+}
+
+const charButtons = document.querySelectorAll('[data-char]')
+const enterButton = document.querySelector('[data-enter]')
+const deleteButton = document.querySelector('[data-delete]')
+const leftButton = document.querySelector('[data-left]')
+const spaceButton = document.querySelector('[data-space]')
+const rightButton = document.querySelector('[data-right]')
+
+charButtons.forEach(button => {
+  button.addEventListener('click', () => {
+  })
+})
+
+enterButton.addEventListener('click', button => {
+})
+
+deleteButton.addEventListener('click', button => {
+})
+
+leftButton.addEventListener('click', button => {
+})
+
+spaceButton.addEventListener('click', button => {
+})
+
+rightButton.addEventListener('click', button => {
+})
+
 
